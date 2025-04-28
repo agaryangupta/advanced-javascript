989e63c5589501498b3b5599d95a1530e66dd33d
agaryangupta <agaryangupta123@gmail.com>
2025-04-28 15:56:15 +0530

    done

--- classes.js
+++ classes.js
@@ -1,14 +1,14 @@
-Classes
+// Classes
 
-class Classname {
-    constructor(prop1, prop2) {
-        this.prop1 = prop1;
-        this.prop2 = prop2;
-    }
-}
-let obj=new Classname("arg1","arg2");
-console.log(obj.prop1);
-console.log(obj.prop2);
+// class Classname {
+//     constructor(prop1, prop2) {
+//         this.prop1 = prop1;
+//         this.prop2 = prop2;
+//     }
+// }
+// let obj=new Classname("arg1","arg2");
+// console.log(obj.prop1);
+// console.log(obj.prop2);
 
 ///////////////////////////////////////////
 
