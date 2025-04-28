13810921f49234e5b82ec440bcc80a5aa987137d
agaryangupta <agaryangupta123@gmail.com>
2025-04-28 15:05:26 +0530

    done

--- classes.js
+++ classes.js
@@ -67,37 +67,48 @@ console.log(obj.prop2);
 
 /******INHERITANCE******** */
 
-class vechile {
-    constructor(color, currentspeed, maxspeed) {
-        this.color = color;
-        this.currentspeed = currentspeed;
-        this.maxspeed = maxspeed;
-    }
-    move() {
-        console.log("Moving at ", this.currentspeed);
+// class vechile {
+//     constructor(color, currentspeed, maxspeed) {
+//         this.color = color;
+//         this.currentspeed = currentspeed;
+//         this.maxspeed = maxspeed;
+//     }
+//     move() {
+//         console.log("Moving at ", this.currentspeed);
 
-    }
-    accelerate(amount) {
-        this.currentspeed += amount;
-    }
-}
-class Motorcycle extends vechile(color, currentspeed, maxspeed, fuel) {
-    constructor(color, currentspeed, maxspeed, fuel) {
-        super(color, currentspeed, maxspeed);
-        this.fuel = fuel;
-    }
+//     }
+//     accelerate(amount) {
+//         this.currentspeed += amount;
+//     }
+// }
+// class Motorcycle extends vechile(color, currentspeed, maxspeed, fuel) {
+//     constructor(color, currentspeed, maxspeed, fuel) {
+//         super(color, currentspeed, maxspeed);
+//         this.fuel = fuel;
+//     }
 
-    doWheelie() {
-        console.log("Driving on one wheel");
+//     doWheelie() {
+//         console.log("Driving on one wheel");
 
-    }
-}
+//     }
+// }
 
-// With the extends keyword we specify that a certain class is the child of another class.
-// The super word in the constructor 
+// // With the extends keyword we specify that a certain class is the child of another class.
+// // The super word in the constructor 
 
 
-let motor =new Motorcycle("Black ",0,250,"gasoline");
-console.log(motor.color); 
-motor.accelerate(50);
-motor.move();
+// let motor =new Motorcycle("Black ",0,250,"gasoline");
+// console.log(motor.color); 
+// motor.accelerate(50);
+// motor.move();
+
+/******PROTOTYPE******** */
+
+Person.prototype.introduce = function() {
+    console.log(Hello, my name is ${this.firstname});
+
+    };
+Person.prototype.favouritecolor = "green";
+let p=new Person("John", "Doe");
+console.log(p.favouritecolor); // Output: green
+p.introduce(); // Output: Hello, my name is John
