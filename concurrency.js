0062439af950678115bc3b3b42387df3390150ad
agaryangupta <agaryangupta123@gmail.com>
2025-05-01 14:24:11 +0530

    done

---
+++ concurrency.js
@@ -0,0 +1,52 @@
+//////callbacks//////
+
+// function dosomething(callback){
+//     callback();
+// }
+// function sayhi(){
+//     console.log("HI");
+// }
+// dosomething(sayhi);
+
+// function judge(grade){
+//     switch(true){
+//         case grade=="A":
+//         console.log("amazing");
+//         break;
+//         case grade=="B":
+//         console.log("amazing");
+//         break;
+//         case grade=="C":
+//         console.log("amazing");
+//         break;
+//         case grade=="D":
+//         console.log("amazing");
+//         break;
+//         default:
+//         console.log("amazing");
+//     }
+// }
+// function getgrade(score,callback){
+//     let grade;
+//     switch(true){
+//         case score=>90:
+//             grade="A";
+//             break;
+//         case score=>80:
+//             grade="B";
+//             break;
+//         case score=>70:
+//             grade="C";
+//             break;
+//         case score=>60:
+//             grade="D";
+//             break; 
+//         default:
+//             grade="F";
+//     }
+//     callback(grade);
+// }
+// getgrade(85,judge);
+
+//////Promises//////
+
