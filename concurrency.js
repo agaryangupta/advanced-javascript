edb9568504d130b7cce596df1e15a1bb9048263d
agaryangupta <agaryangupta123@gmail.com>
2025-05-01 14:49:38 +0530

    done

--- concurrency.js
+++ concurrency.js
@@ -109,3 +109,18 @@
 // .then(value=>{
 //     console.log(value);
 // })
+
+function saysomething(x){
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve("something"+x);
+        },2000);
+    });
+}
+async  function talk(x){
+    const words=await saysomething(x);
+    console.log(words);
+}
+talk(2);
+talk(4);
+talk(8);
